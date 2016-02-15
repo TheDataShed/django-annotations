cdb04b4e5c85505b9797fc51f819c70333c9bf20
datasheded <edthewlis@thedatashed.co.uk>
2016-02-15 09:33:09 +0000

    Corrected typos

--- run-django.sh
+++ run-django.sh
@@ -2,7 +2,7 @@
 
 /bin/python3.4 /code/manage.py migrate --settings=annotator.settings.production
 #echo "Done migration."
-/bin/bash -c "/bin/python3.4 /code/manage.py rqworker default --settings=annotator.settings.production" &
+/bin/bash -c "/bin/python3.4 /code/manage.py rqworker  --settings=annotator.settings.production" &
 #/bin/bash -c "rq-dashboard -H shedis" &
 /bin/python3.4 /code/manage.py runserver 0.0.0.0:8000 --settings=annotator.settings.production
 echo "django app running."
